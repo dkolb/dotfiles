1a512ca4b15f9aa399c5f8c1c6ae88adf6eb09c3
David Kolb <david.kolb@krinchan.com>
2018-01-22 16:31:03 -0500

    Add IAM session option.

--- fish/functions/metalks.fish
+++ fish/functions/metalks.fish
@@ -28,6 +28,8 @@ function metalks --description 'metalks (p|np) (o|c)'
       alks sessions open -a $acct -r $role -o creds -f
     case c
       alks sessions console -a $acct -r $role
+    case i
+      alks sessions open -a $acct -r $role -o creds -f -i
     case '*'
       echo 'Bad arg. metalks (p|np) (c|o)'
       return 1
