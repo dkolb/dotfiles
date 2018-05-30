087e62f295f4bfece5fdb639a9555bf131adf992
David Kolb <david.kolb@krinchan.com>
2018-05-30 09:07:36 -0400

    Fixes, install jenv

--- ubuntu/00_install.sh
+++ ubuntu/00_install.sh
@@ -16,37 +16,42 @@ ap curl
 sudo apt-get -y install $PLIST
 
 # Install rbenv the fun way!
-rm -rf $HOME/.rbenv
-git clone --depth=1 https://github.com/rbenv/rbenv.git $HOME/.rbenv
-mkdir -p $HOME/.rbenv/plugins
-git clone --depth=1 https://github.com/rbenv/ruby-build.git $HOME/.rbenv/plugins
+rm -rf "$HOME/.rbenv"
+git clone --depth=1 'https://github.com/rbenv/rbenv.git' "$HOME/.rbenv"
+mkdir -p "$HOME/.rbenv/plugins"
+git clone --depth=1 'https://github.com/rbenv/ruby-build.git' "$HOME/.rbenv/plugins"
 
 
 # Install nodenv
-rm -rf $HOME/.nodenv
-git clone --depth=1 https://github.com/nodenv/nodenv.git $HOME/.nodenv
-mkdir -p $HOME/.nodenv/plugins
-git clone https://github.com/nodenv/node-build.git $HOME/.nodenv/plugins/node-build
+rm -rf "$HOME/.nodenv"
+git clone --depth=1 'https://github.com/nodenv/nodenv.git' "$HOME/.nodenv"
+mkdir -p "$HOME/.nodenv/plugins"
+git clone 'https://github.com/nodenv/node-build.git' "$HOME/.nodenv/plugins/node-build"
 
 
 # Install tgenv
-rm -rf $HOME/.tgenv
-git clone --depth=1 https://github.com/cunymatthieu/tgenv.git $HOME/.tgenv
+rm -rf "$HOME/.tgenv"
+git clone --depth=1 'https://github.com/cunymatthieu/tgenv.git' "$HOME/.tgenv"
+
+# Install jenv
+rm -rf $HOME/.jenv
+git clone --depth=1 'https://github.com/gcuisinier/jenv.git' "$HOME/.jenv"
+cp "$HOME/.jenv/fish/jenv.fish" "$HOME/.config/fish/functions/jenv.fish"
 
 # Set Fish Shell
-sudo chsh --shell /usr/bin/fish $USER
+sudo chsh --shell /usr/bin/fish "$USER"
 
 REPO_ROOT=$( cd "$(dirname $0)/.."; pwd)
 . "$REPO_ROOT/ubuntu/common_functions.sh"
 
-mkdir -p $HOME/bin
-mkdir -p $HOME/.config
+mkdir -p "$HOME/bin"
+mkdir -p "$HOME/.config"
 
 lf "$REPO_ROOT/fish" "$HOME/.config/fish"
 lf "$REPO_ROOT/omf" "$HOME/.config/omf"
 
-rm -rf $HOME/.local/share/omf
-curl -L https://get.oh-my.fish > /tmp/install_omf.fish
+rm -rf "$HOME/.local/share/omf"
+curl -L 'https://get.oh-my.fish' > /tmp/install_omf.fish
 /usr/bin/fish /tmp/install_omf.fish --noninteractive
 rm /tmp/install_omf.fish
 
