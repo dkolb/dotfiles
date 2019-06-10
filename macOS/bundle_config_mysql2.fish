2b2174c8f67a44fe4b1080a91bcca6cfd4003be0
David Kolb <david.kolb@krinchan.com>
2019-06-10 10:32:02 -0400

    Add bundle config.

--- macOS/bundle_config_mysql2.fish
+++ macOS/bundle_config_mysql2.fish
@@ -1,10 +1,8 @@
 #!/usr/bin/env fish
 
-brew install openssl
-brew install mysql@5.6
+brew install openssl mysql@5.6
 bundle config --global build.mysql2 \
   --with-ldflags=-L/usr/local/opt/openssl/lib \
   --with-cppflags=-I/usr/local/opt/openssl/include \
   --with-mysql-config=/usr/local/Cellar/mysql@5.6/5.6.43/bin/mysql_config \
-  --with-opt-lib=/usr/local/opt/openssl/lib \
-  --srcdir=/usr/local/Cellar/mysql@5.6/5.6.43/include/
+  --with-opt-lib=/usr/local/opt/openssl/lib
